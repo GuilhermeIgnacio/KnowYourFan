ba53f53718efe0446f21e38e06417d2f64ff78dd
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2025-04-26 16:27:58 -0300

    Extracted some elements to different composable for better comprehension

--- composeApp/src/commonMain/kotlin/com/guilherme/knowyourfan/knowyourfan/presentation/SignUpScreen.kt
+++ composeApp/src/commonMain/kotlin/com/guilherme/knowyourfan/knowyourfan/presentation/SignUpScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -35,6 +36,7 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -80,7 +82,7 @@ fun SignUpScreen() {
             .padding(horizontal = 8.dp)
             .statusBarsPadding()
             .navigationBarsPadding(),
-        verticalArrangement = Arrangement.spacedBy(32.dp)
+        verticalArrangement = Arrangement.spacedBy(6.dp)
     ) {
 
         Image(
@@ -91,251 +93,272 @@ fun SignUpScreen() {
             contentDescription = ""
         )
 
-        Column(
-            verticalArrangement = Arrangement.spacedBy(6.dp)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TextFields(iconsColor, outlinedTextFieldColors)
+
+        IdSection(placeholderTextColor, outlinedTextFieldContainerColor)
+
+        InterestGamesSection(placeholderTextColor)
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Max),
+            onClick = {/*Todo Sign In*/ },
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors().copy(
+                contentColor = Color.Black,
+                containerColor = Color.White,
+            )
         ) {
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = { },
-                placeholder = { Text(text = "Username") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Person,
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                maxLines = 1,
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = { },
-                placeholder = { Text(text = "Email") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Email,
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                maxLines = 1,
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = { },
-                placeholder = { Text(text = "Confirm Email") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Email,
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                maxLines = 1,
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = "",
-                onValueChange = { },
-                placeholder = { Text(text = "CPF") },
-                leadingIcon = {
-                    Icon(
-                        modifier = Modifier.size(24.dp),
-                        imageVector = vectorResource(Res.drawable.address_card_regular),
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                maxLines = 1,
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = "",
-                onValueChange = {},
-                placeholder = { Text(text = "Password") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Lock,
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                trailingIcon = {},
-                maxLines = 1,
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                value = "",
-                onValueChange = {},
-                placeholder = { Text(text = "Confirm Password") },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Lock,
-                        contentDescription = "",
-                        tint = iconsColor
-                    )
-                },
-                trailingIcon = {},
-                maxLines = 1,
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                shape = RoundedCornerShape(12.dp),
-                colors = outlinedTextFieldColors,
-            )
-
-            Text(
-                text = "Upload your ID photo",
-                color = placeholderTextColor
-            )
-
-            Column(
-                modifier = Modifier
-                    .dashedBorder(
-                        width = 1.dp,
-                        brush = Brush.horizontalGradient(
-                            listOf(
-                                placeholderTextColor,
-                                placeholderTextColor
-                            )
-                        ),
-                        shape = RoundedCornerShape(12.dp), on = 8.dp, off = 8.dp
-                    )
-                    .padding(all = 24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                Box(
-                    modifier = Modifier
-                        .background(color = outlinedTextFieldContainerColor, shape = CircleShape)
-                ) {
-                    Icon(
-                        imageVector = vectorResource(Res.drawable.image_solid),
-                        contentDescription = "",
-                        modifier = Modifier
-                            .padding(12.dp)
-                            .size(24.dp)
-                    )
-                }
-
-                Text(
-                    text = "Don't worry, your data won't be stored. It only will be used for verification",
-                    textAlign = TextAlign.Center,
-                    color = placeholderTextColor
-                )
-
-                OutlinedButton(
-                    onClick = {/*Todo: pick photo*/ },
-                    shape = RoundedCornerShape(12.dp)
-                ) {
-                    Text(text = "Submit")
-                }
-
-            }
-
-            val games = listOf(
-                ChipItem(
-                    game = "Counter-Strike",
-                    onClick = {},
-                    icon = null
-                ),
-
-                ChipItem(
-                    game = "Valorant",
-                    onClick = {},
-                    icon = null
-                ),
-                ChipItem(
-                    game = "Rainbow Six",
-                    onClick = {},
-                    icon = null
-                ),
-                ChipItem(
-                    game = "Apex Legends",
-                    onClick = {},
-                    icon = null
-                ),
-                ChipItem(
-                    game = "League of Legends",
-                    onClick = {},
-                    icon = null
-                ),
-                ChipItem(
-                    game = "Fighting Games",
-                    onClick = {},
-                    icon = null
-                ),
-            )
-
-            Text(
-                text = "Select games you`re interested in",
-                color = placeholderTextColor
-            )
-            FlowRow(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-
-                games.forEach {
-                    FilterChip(
-                        onClick = { },
-                        label = {
-                            Text(text = it.game)
-                        },
-                        shape = RoundedCornerShape(100),
-                        selected = false,
-                        leadingIcon = if (it.icon != null) {
-                            {
-                                Icon(
-                                    imageVector = it.icon,
-                                    contentDescription = "",
-                                    modifier = Modifier.size(FilterChipDefaults.IconSize)
-                                )
-                            }
-                        } else {
-                            null
-                        },
-                    )
-                }
-
-            }
-
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(IntrinsicSize.Max),
-                onClick = {/*Todo Sign In*/ },
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors().copy(
-                    contentColor = Color.Black,
-                    containerColor = Color.White,
-                )
-            ) {
-                Text(text = "Register")
-            }
-
+            Text(text = "Register")
         }
+
+
     }
+}
+
+@Composable
+@OptIn(ExperimentalLayoutApi::class)
+private fun InterestGamesSection(placeholderTextColor: Color) {
+    val games = listOf(
+        ChipItem(
+            game = "Counter-Strike",
+            onClick = {},
+            icon = null
+        ),
+
+        ChipItem(
+            game = "Valorant",
+            onClick = {},
+            icon = null
+        ),
+        ChipItem(
+            game = "Rainbow Six",
+            onClick = {},
+            icon = null
+        ),
+        ChipItem(
+            game = "Apex Legends",
+            onClick = {},
+            icon = null
+        ),
+        ChipItem(
+            game = "League of Legends",
+            onClick = {},
+            icon = null
+        ),
+        ChipItem(
+            game = "Fighting Games",
+            onClick = {},
+            icon = null
+        ),
+    )
+
+    Text(
+        text = "Select games you`re interested in",
+        color = placeholderTextColor
+    )
+    FlowRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+
+        games.forEach {
+            FilterChip(
+                onClick = { },
+                label = {
+                    Text(text = it.game)
+                },
+                shape = RoundedCornerShape(100),
+                selected = false,
+                leadingIcon = if (it.icon != null) {
+                    {
+                        Icon(
+                            imageVector = it.icon,
+                            contentDescription = "",
+                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+                        )
+                    }
+                } else {
+                    null
+                },
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun IdSection(
+    placeholderTextColor: Color,
+    outlinedTextFieldContainerColor: Color,
+) {
+    Text(
+        text = "Upload your ID photo",
+        color = placeholderTextColor
+    )
+
+    Column(
+        modifier = Modifier
+            .dashedBorder(
+                width = 1.dp,
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        placeholderTextColor,
+                        placeholderTextColor
+                    )
+                ),
+                shape = RoundedCornerShape(12.dp), on = 8.dp, off = 8.dp
+            )
+            .padding(all = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = outlinedTextFieldContainerColor, shape = CircleShape)
+        ) {
+            Icon(
+                imageVector = vectorResource(Res.drawable.image_solid),
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(24.dp)
+            )
+        }
+
+        Text(
+            text = "Don't worry, your data won't be stored. It only will be used for verification",
+            textAlign = TextAlign.Center,
+            color = placeholderTextColor
+        )
+
+        OutlinedButton(
+            onClick = {/*Todo: pick photo*/ },
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text(text = "Submit")
+        }
+
+    }
+}
+
+@Composable
+private fun TextFields(
+    iconsColor: Color,
+    outlinedTextFieldColors: TextFieldColors,
+) {
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = "",
+        onValueChange = { },
+        placeholder = { Text(text = "Username") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Person,
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        maxLines = 1,
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = "",
+        onValueChange = { },
+        placeholder = { Text(text = "Email") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Email,
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        maxLines = 1,
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = "",
+        onValueChange = { },
+        placeholder = { Text(text = "Confirm Email") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Email,
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        maxLines = 1,
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
+
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = "",
+        onValueChange = { },
+        placeholder = { Text(text = "CPF") },
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.size(24.dp),
+                imageVector = vectorResource(Res.drawable.address_card_regular),
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        maxLines = 1,
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        value = "",
+        onValueChange = {},
+        placeholder = { Text(text = "Password") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Lock,
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        trailingIcon = {},
+        maxLines = 1,
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        value = "",
+        onValueChange = {},
+        placeholder = { Text(text = "Confirm Password") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Lock,
+                contentDescription = "",
+                tint = iconsColor
+            )
+        },
+        trailingIcon = {},
+        maxLines = 1,
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        shape = RoundedCornerShape(12.dp),
+        colors = outlinedTextFieldColors,
+    )
 }
 
 data class ChipItem(
