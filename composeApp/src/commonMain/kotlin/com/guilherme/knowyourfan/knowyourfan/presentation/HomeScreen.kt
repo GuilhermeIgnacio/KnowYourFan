3259a017cbd8b03e25ed4c0fc9de95233a169e6d
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2025-05-01 22:46:42 -0300

    Home Screen UI changes

--- composeApp/src/commonMain/kotlin/com/guilherme/knowyourfan/knowyourfan/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/guilherme/knowyourfan/knowyourfan/presentation/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.guilherme.knowyourfan.knowyourfan.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,12 +16,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +36,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import knowyourfan.composeapp.generated.resources.Res
+import knowyourfan.composeapp.generated.resources.furia_logo
 import knowyourfan.composeapp.generated.resources.x_twitter_brands
+import org.jetbrains.compose.resources.imageResource
 import org.jetbrains.compose.resources.vectorResource
 
 @Composable
@@ -40,131 +47,131 @@ fun HomeScreen(viewModel: HomeViewModel) {
     val state by viewModel.state.collectAsStateWithLifecycle()
     val onEvent = viewModel::onEvent
 
-    Column(
-        modifier = Modifier
-            .statusBarsPadding()
-            .navigationBarsPadding()
-            .padding(vertical = 16.dp, horizontal = 8.dp)
-    ) {
 
-        Spacer(Modifier.height(16.dp))
 
-        if (!state.isLinkedWithX) {
-            LinkAccount(
-                modifier = Modifier.weight(1f),
-                onEvent = onEvent
-            )
-        } else {
+    Spacer(Modifier.height(16.dp))
 
-            /*Button(onClick = { viewModel.getRecommendations() }) {
-                Text("Generate Recommendations")
-            }
+    if (!state.isLinkedWithX) {
+        LinkAccount(
+            onEvent = onEvent
+        )
+    } else {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth().statusBarsPadding().navigationBarsPadding(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
 
-            LazyColumn {
-                items(state.recommendations) {
-                    HorizontalDivider()
-                    Text(it.title)
-                    Text(it.link)
-                    HorizontalDivider()
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
 
-                }
-            }*/
-
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(state.recommendations) {
-                    Card(
-                        modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Max),
-                        onClick = { onEvent(HomeEvents.OnCardClicked(it.link)) },
-                        shape = RoundedCornerShape(12.dp),
-                        colors = CardDefaults.cardColors()
-                            .copy(containerColor = Color(0xFF1f2937), contentColor = Color.White)
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(12.dp),
-                            text = it.title,
-                            fontWeight = FontWeight.Bold,
-                            style = MaterialTheme.typography.titleLarge
+                    IconButton(onClick = {}) {
+                        Image(
+                            modifier = Modifier.size(26.dp),
+                            bitmap = imageResource(Res.drawable.furia_logo),
+                            contentDescription = ""
                         )
-                        HorizontalDivider()
-                        Text(
-                            modifier = Modifier.padding(12.dp),
-                            text = it.link,
-                            fontWeight = FontWeight.Bold,
-                            style = MaterialTheme.typography.titleLarge
+                    }
+
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.Person,
+                            contentDescription = ""
                         )
                     }
                 }
             }
 
-
+            items(state.recommendations) {
+                Card(
+                    modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Max),
+                    onClick = { onEvent(HomeEvents.OnCardClicked(it.link)) },
+                    shape = RoundedCornerShape(12.dp),
+                    colors = CardDefaults.cardColors()
+                        .copy(containerColor = Color(0xFF1f2937), contentColor = Color.White)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        text = it.title,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
         }
+
 
     }
 }
 
 @Composable
 private fun LinkAccount(
-    modifier: Modifier = Modifier,
     onEvent: (HomeEvents) -> Unit,
 ) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        text = "Link your X account",
-        style = MaterialTheme.typography.titleLarge,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
-    )
-
-    Spacer(Modifier.height(8.dp))
-
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        text = "Get better personalized information and recommendations",
-        textAlign = TextAlign.Center
-    )
-
-    Spacer(modifier)
-
     Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        modifier = Modifier
+            .statusBarsPadding()
+            .navigationBarsPadding()
+            .padding(vertical = 16.dp, horizontal = 8.dp)
     ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Link your X account",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
 
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(IntrinsicSize.Max),
-            onClick = { onEvent(HomeEvents.OnLinkWithXButtonClicked) },
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors().copy(
-                contentColor = Color.Black,
-                containerColor = Color.White,
-            )
+        Spacer(Modifier.height(8.dp))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Get better personalized information and recommendations",
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(Modifier.weight(1f))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Icon(
-                modifier = Modifier.size(24.dp),
-                imageVector = vectorResource(Res.drawable.x_twitter_brands),
-                contentDescription = ""
-            )
 
-            Spacer(Modifier.width(8.dp))
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max),
+                onClick = { onEvent(HomeEvents.OnLinkWithXButtonClicked) },
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors().copy(
+                    contentColor = Color.Black,
+                    containerColor = Color.White,
+                )
+            ) {
+                Icon(
+                    modifier = Modifier.size(24.dp),
+                    imageVector = vectorResource(Res.drawable.x_twitter_brands),
+                    contentDescription = ""
+                )
 
-            Text(text = "Continue With X")
-        }
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(IntrinsicSize.Max),
-            onClick = { onEvent(HomeEvents.OnLinkWithXButtonClicked) },
-            shape = RoundedCornerShape(12.dp),
-            colors = ButtonDefaults.buttonColors().copy(
-                contentColor = Color.White,
-                containerColor = Color(0xFF1f2937),
-            )
-        ) {
-            Text(text = "Continue Without X")
+                Spacer(Modifier.width(8.dp))
+
+                Text(text = "Continue With X")
+            }
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Max),
+                onClick = { onEvent(HomeEvents.OnLinkWithXButtonClicked) },
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors().copy(
+                    contentColor = Color.White,
+                    containerColor = Color(0xFF1f2937),
+                )
+            ) {
+                Text(text = "Continue Without X")
+            }
         }
     }
 }
