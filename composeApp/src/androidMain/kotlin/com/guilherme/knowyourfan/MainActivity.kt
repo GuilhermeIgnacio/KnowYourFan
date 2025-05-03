29049219667ff5d4096eedefb6370a33995ededa
GuilhermeIgnácio <stormierearth9@gmail.com>
2025-05-03 03:47:57 -0300

    Lock device orientation

--- composeApp/src/androidMain/kotlin/com/guilherme/knowyourfan/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/guilherme/knowyourfan/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.guilherme.knowyourfan
 
+import android.app.Activity
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.guilherme.knowyourfan.app.App
 import com.guilherme.knowyourfan.di.initKoin
@@ -17,6 +20,10 @@ class MainActivity : ComponentActivity() {
 
         initKoin(config = null, activity = this@MainActivity)
         setContent {
+
+            val context = LocalContext.current
+            (context as Activity)?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
                 AppTheme {
                     App()
                 }
